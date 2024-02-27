60f9417918124b187fe505fbab9bb97a751a33ae
Alan <sz9751210@gmail.com>
2024-02-27 17:03:28 +0800

    booklist 初版

---
+++ src/api/books.js
@@ -0,0 +1,19 @@
+export const books = [
+  {
+    title: '鳳凰專案｜看 IT部門如何讓公司從谷底翻身的傳奇故事',
+    coverUrl: 'https://cf-assets2.tenlong.com.tw/products/images/000/110/186/original/9789864765867.jpg?1525539935',
+    authors: ['Gene Kim', 'Kevin Behr'], // 现在是一个数组
+    content: '看 IT部門如何讓公司從谷底翻身的傳奇故事',
+    tag: ['devops'],
+    url: 'https://example.com',
+  },
+  {
+    title: '學徒模式－優秀軟體開發者的養成之路',
+    coverUrl: 'https://cf-assets1.tenlong.com.tw/images/64457/original/A277.jpg',
+    authors: ['Dave Hoover', 'Adewale Oshineye'], // 现在是一个数组
+    content:
+      '你是否盡力延長作為軟體開發人員的職涯？在今日快速變化與技術的持續擴展，成功需要的不只是對技術的精通，為了專業地成長，你還需要軟性技能與有效學習的能力，本書內容是關於如何精進這些技能。Dave Hoover與Adewale Oshineye兩位作者收錄了許多行為模式，能幫助你讓個人技藝的各個基本面向更加完善。收錄了多年的研究、許多面談訪問以及歐萊禮線上論壇的回應，書中的模式包含了程式設計師、系統管理人員與設計師每天都會接觸到的困難狀況。這並不僅與財務上的成功有關，學徒模式也把軟體開發視作個人滿足的工具之一，閱讀本書能幫助你在生活與職涯兩方面都獲得最好的結果。',
+    tag: ['self-development'],
+    url: 'https://example.com',
+  },
+]
--- src/views/devops/BookList.vue
+++ src/views/devops/BookList.vue
@@ -1,56 +1,128 @@
 <template>
-  <div>
-    <div class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
+  <div class="select-container">
+    <n-select
+      v-model:value="selectedAuthorTags"
+      :options="authorOptions"
+      placeholder="選擇作者"
+      multiple
+      style="margin-bottom: 20px; width: 100%"
+      class="select-item"
+    />
+    <n-select
+      v-model:value="selectedTags"
+      :options="tagOptions"
+      placeholder="選擇分類"
+      multiple
+      style="margin-bottom: 20px; width: 100%"
+      class="select-item"
+    />
+  </div>
+  <div class="cards-container">
+    <n-card v-for="(card, index) in filteredCards" :key="index" class="card-item" @click="navigateTo(card.url)">
+      <template #cover>
+        <img :src="card.coverUrl" />
+      </template>
+      <template #header>
+        <!-- 包裹标题以应用样式 -->
+        <div class="card-title">{{ card.title }}</div>
+      </template>
+      <div class="tags-container">
+        <n-tag v-for="(author, index) in card.authors" :key="`author-${index}`" type="info">
+          {{ author }}
+        </n-tag>
+      </div>
+      <div class="tags-container">
+        <n-tag v-for="(tag, index) in card.tag" :key="`tag-${index}`" type="success">
+          {{ tag }}
+        </n-tag>
+      </div>
+      <!-- {{ card.content }} -->
+    </n-card>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.content-box {
-  background-color: #fff;
-  border-radius: 15px;
-  padding: 20px;
+<script setup>
+import { NCard, NSelect, NTag } from 'naive-ui'
+import { ref, computed } from 'vue'
+import { books } from '@/api/books'
+
+const cards = ref(books)
+
+const selectedAuthorTags = ref([])
+const selectedTags = ref([])
+
+// 提取所有唯一的标签来作为选择器的选项
+const tagOptions = computed(() => {
+  const allTags = new Set(cards.value.flatMap((card) => card.tag))
+  return Array.from(allTags).map((tag) => ({ label: tag, value: tag }))
+})
+
+const authorOptions = computed(() => {
+  const allAuthors = new Set(cards.value.flatMap((card) => card.authors))
+  return Array.from(allAuthors).map((author) => ({ label: author, value: author }))
+})
+
+// Filter cards based on selected tags and author tags
+const filteredCards = computed(() => {
+  return cards.value.filter(
+    (card) =>
+      (selectedTags.value.length === 0 || card.tag.some((tag) => selectedTags.value.includes(tag))) &&
+      (selectedAuthorTags.value.length === 0 ||
+        card.authors.some((author) => selectedAuthorTags.value.includes(author)))
+  )
+})
+
+// 点击卡片时执行的函数
+const navigateTo = (url) => {
+  window.location.href = url
+}
+</script>
+
+<style scoped>
+.select-container {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px; /* 根据需要调整 */
+  margin-bottom: 20px;
+}
+.select-item {
+  width: calc(50% - 10px); /* 减去间隙的宽度 */
+}
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: start; /* 这将在卡片之间提供一些空间，并且在行的末尾对齐卡片 */
+  gap: 10px;
+}
+.card-title {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.2em; /* 根据实际情况调整 */
+  max-height: 2.4em; /* 根据实际情况调整，这里是 line-height 的两倍 */
+  word-break: break-word;
+}
+
+.card-item {
+  margin-bottom: 20px; /* 每个卡片底部的间距 */
+  max-width: calc(33.3333% - 110px); /* 根据需要调整这里的 10px 以控制卡片之间的空间 */
+  cursor: pointer; /* 表示卡片可点击 */
+  transition: box-shadow 0.3s ease; /* 平滑过渡效果 */
+}
+
+.card-item:hover {
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* Hover 效果 */
+}
+/* .tags-container {
+  margin: 10px 0;
+} */
+
+.tags-container .n-tag {
+  padding: 0 4px; /* 减小内边距 */
+  font-size: 12px; /* 减小字体大小 */
+  margin-right: 5px;
+  margin-top: 10px;
 }
 </style>
